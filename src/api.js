ec8f422c7847cc12a91f02119ed49c538183d4e0
atif <atifsial5510@gmail.com>
2026-05-15 16:50:27 +0500

    added api suffix

--- src/api.js
+++ src/api.js
@@ -1,1 +1,1 @@
-export const API_BASE = import.meta.env.VITE_API_URL || 'https://khidmat-official-v1.vercel.app/api';
+export const API_BASE = (import.meta.env.VITE_API_URL || 'https://khidmat-official-v1.vercel.app') + '/api';
